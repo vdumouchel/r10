b39421ad2b6a71eba6fd62225fbf3ee44f0a2c9b
vdumouchel <vudmouchel@me.com>
2019-05-28 12:39:39 -0400

    done adding the styles to SessionScreen

--- components/screens/SessionScreen.js
+++ components/screens/SessionScreen.js
@@ -8,13 +8,40 @@ import moment from 'moment-timezone'
 import AsyncStorage from '@react-native-community/async-storage'
 
 // react-native imports
-import { ScrollView, View, Text } from 'react-native'
+import { ScrollView, View, Text, Image } from 'react-native'
 import styles from '../../assets/styles/styles'
 import _ from 'lodash'
 
 // CSS & Style imports
-import Icon from 'react-native-vector-icons/FontAwesome5'
+import Icon from 'react-native-vector-icons/FontAwesome'
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import LinearGradient from 'react-native-linear-gradient';
+
+
+const Session = gql`
+ query Session($sessionKey: ID!){
+   Session(id: $sessionKey ){
+    id
+    title
+    description
+    location
+    speaker {
+      id
+      name
+      image
+      session{
+        id
+        location
+        startTime
+        title
+      }
+    }
+    startTime
+  }
+}
+`;
+
+
 
 const SessionScreen = props => {
 
@@ -25,34 +52,83 @@ const SessionScreen = props => {
 			title: navigation.getParam('title'),
 		}
 	}
-
-	// declare variable
+	// retrieve data from Async Storage
 	let [sessionKey, setSelectedSession] = useState('')
 
 	useEffect(() => {
 		_retrieveData()
 	}, [])
 
-	_retrieveData = async defaultValue => {
+	_retrieveData = async () => {
 		try {
-			sessionKey = await AsyncStorage.getItem('selectedSession')
-			setSelectedSession(sessionKey)
-			if (sessionKey !== null) {
-				console.log('this is selectSession: ', sessionKey)
-				return sessionKey
+			var tempKey = await AsyncStorage.getItem('selectedSession')
+			setSelectedSession(tempKey)
+			if (tempKey !== null) {
+				console.log('this is sessionKey: ', tempKey)
+				return tempKey
 			}
 		} catch (e) {
 			throw e.message
 		}
 	}
-
-
+	console.log("SessionKey: ", sessionKey)
+	const { data, error, loading } = useQuery(Session, { variables: { sessionKey } })
+	// let dataThisSession = data.thisSession
+	// console.log('this is dataThisSession: ', dataThisSession)
+	if (loading) {
+		return <Text>Loading...</Text>
+	}
+	if (error) {
+		return <Text>Error! {error.message}</Text>
+	}
+	console.log('this is dataThisSession: ', data.Session.speaker.image)
 
 	return (
 		<View style={styles.container}>
-			<Text>This is Session Screen!</Text>
-			<Text>{sessionKey}</Text>
-			<Icon name='dog' size={30} color='#900' />
+			<View style={styles.sessionContainer}>
+				<View>
+					<Text style={styles.sessionLocation}>{data.Session.location}</Text>
+				</View>
+				<View>
+					<Text style={styles.sessionTitle}>{data.Session.title}</Text>
+				</View>
+				<View>
+					<Text style={styles.sessionStartTime}>{moment(data.Session.startTime).subtract(3, 'hours').format('LT')}</Text>
+				</View>
+				<View>
+					<Text style={styles.sessionDescription}>{data.Session.description}</Text>
+				</View>
+				<View>
+					<Text style={styles.sessionPresentedBy}>Presented by:</Text>
+				</View>
+				<View>
+					<TouchableOpacity style={styles.sessionSpeaker}>
+						<Image source={{ uri: data.Session.speaker.image }} style={styles.sessionSpeakerImage}></Image>
+						<Text style={styles.sessionSpeakerText}>{data.Session.speaker.name}</Text>
+					</TouchableOpacity>
+				</View>
+				<View style={styles.sessionHorizontalRuler} />
+				{/* <View style={styles.sessionFavesView}>
+					<TouchableOpacity style={styles.sessionFavesTouchable}>
+						<LinearGradient colors={['#C5414D', '#9963E9']}
+							start={{ x: 0, y: 0 }}
+							end={{ x: 2, y: 1 }}>
+							<Text style={styles.sessionFavesText}>
+								Add to Faves</Text>
+						</LinearGradient>
+					</TouchableOpacity> */}
+				{/* </View> */}
+				<View style={styles.sessionFavesContainer}>
+					<TouchableOpacity>
+						<LinearGradient colors={['#9963EA', '#8F80DF', '#8797D6']} style={styles.sessionFavesLinearGradient} start={{ x: 0, y: -0.4 }}
+							end={{ x: 1.2, y: 1.5 }}>
+							<Text style={styles.sessionFavesButton}>
+								Add to Faves
+              </Text>
+						</LinearGradient>
+					</TouchableOpacity>
+				</View>
+			</View>
 		</View>
 	)
 }
