d8fbe2a702eeebb2deae8cb3d5572245d601d3ef
vdumouchel <vudmouchel@me.com>
2019-05-27 21:28:27 -0400

    async storage of session key works between scheduleScreen and SessionScreen

--- components/screens/ScheduleScreen.js
+++ components/screens/ScheduleScreen.js
@@ -4,13 +4,18 @@ import React, { useState } from 'react'
 //packages imports
 import gql from 'graphql-tag'
 import { useQuery } from 'react-apollo-hooks'
+import moment from 'moment-timezone'
+import AsyncStorage from '@react-native-community/async-storage';
 
 // react-native imports
-import { StyleSheet, View, Text } from 'react-native'
+import { ScrollView, View, Text } from 'react-native'
 import styles from '../../assets/styles/styles'
+import _ from 'lodash'
 
 // CSS & Style imports
 import Icon from 'react-native-vector-icons/FontAwesome5'
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
 
 const allSessions = gql`
 	{
@@ -19,37 +24,93 @@ const allSessions = gql`
 			title
 			description
 			location
-			speaker
 			startTime
 		}
 	}
 `
 
 const ScheduleScreen = props => {
+	// const [session, setSession] = useState([])
+
+	// _renderSessions = likedSession => {
+	// 	setSession(likedSession)
+	// }
+
+	// _renderHeader = (session, index, isActive) => {
+	// 	return (
+	// 		<View>
+	// 			<Text>{session.startTime}</Text>
+	// 		</View>
+	// 	)
+	// }
+
+	const { data, error, loading } = useQuery(allSessions)
+	let dataAllSessions = data.allSessions
+	let groups = _.groupBy(dataAllSessions, 'startTime')
+	let newArrayofGroups = (Object.values(groups))
+
+
+
+	if (loading) {
+		return <Text>Loading...</Text>
+	}
+	if (error) {
+		return <Text>Error! {error.message}</Text>
+	}
 	return (
-		<View style={styles.container}>
-			<Text style={styles.welcome}>This will be the Schedule Screen!</Text>
-			<Icon name='calendar' size={30} color='#900' />
-			<View style={styles.iconButton}>
-				<Icon.Button
-					name='sitemap'
-					title='Session'
-					onPress={() => {
-						// try {
-						// 	await AsyncStorage.setItem('storage', value1)
-						// 	console.log('this is AsyncStorage: ', value1)
-						// } catch (e) {
-						// 	throw e.message
-						// }
-						props.navigation.navigate('Session', {
-							title: 'Session',
-						})
-					}}
-				>
-					Session
-				</Icon.Button>
-			</View>
-		</View>
+		<ScrollView>
+			{newArrayofGroups.map((timeSlot, index) => {
+				if (timeSlot.length === 1) {
+					return (
+						<View style={styles.container} key={index}>
+							<Text style={styles.scheduleTime}>{moment(timeSlot[0].startTime).subtract(3, 'hours').format('LT')}</Text>
+							{timeSlot.map(session => (
+								<TouchableOpacity key={session.id} onPress={async () => {
+									try {
+										AsyncStorage.setItem('selectedSession', session.id)
+										console.log('this is AsyncStorage SelectedSession: ', session.id)
+
+									} catch (e) {
+										throw e.message
+									}
+									props.navigation.navigate('Session', {
+										title: 'Session',
+									})
+								}}>
+									<Text style={styles.scheduleTitle}>{session.title}</Text>
+									<Text style={styles.scheduleLocation}>{session.location}</Text>
+								</TouchableOpacity>
+							))}
+						</View>
+					)
+				} else {
+
+					return (
+						<View style={styles.container} key={index}>
+							<Text style={styles.scheduleTime}>{moment(timeSlot[0].startTime).subtract(3, 'hours').format('LT')}</Text>
+							{timeSlot.map(session => (
+								<TouchableOpacity key={session.id} onPress={async () => {
+									try {
+										AsyncStorage.setItem('selectedSession', session.id)
+										console.log('this is AsyncStorage: ', session.id)
+
+									} catch (e) {
+										throw e.message
+									}
+									props.navigation.navigate('Session', {
+										title: 'Session',
+									})
+								}} >
+									<Text style={styles.scheduleTitle}>{session.title}</Text>
+									<Text style={styles.scheduleLocation}>{session.location}</Text>
+									<View style={styles.allWidthRuler}></View>
+								</TouchableOpacity>
+							))}
+						</View>
+					)
+				}
+			})}
+		</ScrollView>
 	)
 }
 
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import App from './App'
 import { name as appName } from './app.json'
 import { YellowBox } from 'react-native'
 
+
 YellowBox.ignoreWarnings(['Remote debugger'])
 
 AppRegistry.registerComponent(appName, () => App)
